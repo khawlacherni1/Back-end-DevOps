02a4269b6f8ed8b38ec3821693e5c0eba011a9a4
anas <anas.ouaghrem@esprit.tn>
2022-11-03 17:23:52 +0100

    fix tests

--- src/main/java/tn/esprit/rh/achat/entities/CategorieProduit.java
+++ src/main/java/tn/esprit/rh/achat/entities/CategorieProduit.java
@@ -9,12 +9,11 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Getter
+@lombok.Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
